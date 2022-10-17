ea1e16d7a8a243669d62f4ded58ab9525af58add
Trung Ho <trung_ho@epam.com>
2022-10-17 23:05:57 +0700

    HW1-Complete Task1, Task2, Task3

---
+++ constants.js
@@ -0,0 +1,7 @@
+const PATHS = {
+  CSV_PATH: './csv/nodejs-hw1-ex1.csv',
+  CSV_TEXT2_PATH: './text/nodejs-hw1-ex2.txt',
+  CSV_TEXT3_PATH: './text/nodejs-hw1-ex3.txt',
+};
+
+module.exports = PATHS;
--- task3.js
+++ task3.js
@@ -4,9 +4,9 @@ const csv = require('csvtojson');
 const PATHS = require('./constants');
 
 const CSV_PATH = PATHS.CSV_PATH;
-const CSV_TEXT2_PATH = PATHS.CSV_TEXT2_PATH;
+const CSV_TEXT3_PATH = PATHS.CSV_TEXT3_PATH;
 
-const writeText = fs.createWriteStream(CSV_TEXT2_PATH);
+const writeText = fs.createWriteStream(CSV_TEXT3_PATH);
 
 const rl = readline.createInterface({
   input: csv().fromFile(CSV_PATH),
